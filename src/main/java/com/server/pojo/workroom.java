ed4941a71b0044e91f041169b536ccc688efeccb
eastCityZheng <773770532@qq.com>
2018-06-12 20:47:26 +0800

    The first.1 commit

---
+++ src/main/java/com/server/pojo/workroom.java
@@ -0,0 +1,23 @@
+package com.server.pojo;
+
+public class workroom {
+
+    private int w_id;
+    private String  w_name;
+
+    public int getW_id() {
+        return w_id;
+    }
+
+    public void setW_id(int w_id) {
+        this.w_id = w_id;
+    }
+
+    public String getW_name() {
+        return w_name;
+    }
+
+    public void setW_name(String w_name) {
+        this.w_name = w_name;
+    }
+}
