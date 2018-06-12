f82d8a95269eb498e910d65044fb40cb39b73814
eastCityZheng <773770532@qq.com>
2018-06-12 20:20:19 +0800

    The first commit

---
+++ src/main/java/com/server/pojo/User.java
@@ -0,0 +1,77 @@
+package com.server.pojo;
+
+public class User {
+
+        private int u_id;
+        private int w_id;
+        private String u_account;
+        private String u_password;
+        private String u_name;
+        private String u_class;
+        private String u_grade;
+        private int u_type;
+
+    public int getU_id() {
+        return u_id;
+    }
+
+    public void setU_id(int u_id) {
+        this.u_id = u_id;
+    }
+
+    public int getW_id() {
+        return w_id;
+    }
+
+    public void setW_id(int w_id) {
+        this.w_id = w_id;
+    }
+
+    public String getU_account() {
+        return u_account;
+    }
+
+    public void setU_account(String u_account) {
+        this.u_account = u_account;
+    }
+
+    public String getU_password() {
+        return u_password;
+    }
+
+    public void setU_password(String u_password) {
+        this.u_password = u_password;
+    }
+
+    public String getU_name() {
+        return u_name;
+    }
+
+    public void setU_name(String u_name) {
+        this.u_name = u_name;
+    }
+
+    public String getU_class() {
+        return u_class;
+    }
+
+    public void setU_class(String u_class) {
+        this.u_class = u_class;
+    }
+
+    public String getU_grade() {
+        return u_grade;
+    }
+
+    public void setU_grade(String u_grade) {
+        this.u_grade = u_grade;
+    }
+
+    public int getU_type() {
+        return u_type;
+    }
+
+    public void setU_type(int u_type) {
+        this.u_type = u_type;
+    }
+}
